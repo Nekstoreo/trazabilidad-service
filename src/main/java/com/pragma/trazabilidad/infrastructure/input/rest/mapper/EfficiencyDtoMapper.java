4741a5781071c9312357ee364ad8ceb48b58f0cf
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-06 17:07:23 -0500

    feat: add EfficiencyDtoMapper and TraceabilityDtoMapper for data transformation

---
+++ src/main/java/com/pragma/trazabilidad/infrastructure/input/rest/mapper/EfficiencyDtoMapper.java
@@ -0,0 +1,20 @@
+package com.pragma.trazabilidad.infrastructure.input.rest.mapper;
+
+import com.pragma.trazabilidad.domain.model.EmployeeRanking;
+import com.pragma.trazabilidad.domain.model.OrderEfficiency;
+import com.pragma.trazabilidad.infrastructure.input.rest.dto.EmployeeRankingResponseDto;
+import com.pragma.trazabilidad.infrastructure.input.rest.dto.OrderEfficiencyResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface EfficiencyDtoMapper {
+    
+    OrderEfficiencyResponseDto toOrderEfficiencyDto(OrderEfficiency orderEfficiency);
+    List<OrderEfficiencyResponseDto> toOrderEfficiencyDtoList(List<OrderEfficiency> orderEfficiencyList);
+    
+    EmployeeRankingResponseDto toEmployeeRankingDto(EmployeeRanking employeeRanking);
+    List<EmployeeRankingResponseDto> toEmployeeRankingDtoList(List<EmployeeRanking> employeeRankingList);
+}
