50a5868bc8490aaec59998ca587893ec3cff5fa3
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-06 14:04:52 -0500

    chore: update Gradle and Spring Boot versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.0"
+    id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -9,7 +9,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
@@ -27,34 +27,32 @@ extra.apply {
     set("springdocVersion", "3.0.0")
     set("mapstructVersion", "1.6.2")
     set("jjwtVersion", "0.12.5")
-    set("lombokVersion", "1.18.34")
+    set("lombokVersion", "1.18.42")
     set("lombokMapstructBindingVersion", "0.2.0")
 }
 
 dependencies {
-    // Spring Boot Starters
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    
-    // OpenAPI / Swagger
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springdocVersion")}")
-    
-    // MapStruct
+    // Implementation
+    implementation("io.jsonwebtoken:jjwt-api:${property("jjwtVersion")}")
     implementation("org.mapstruct:mapstruct:${property("mapstructVersion")}")
-    annotationProcessor("org.mapstruct:mapstruct-processor:${property("mapstructVersion")}")
-    
-    // Lombok
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springdocVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Compile Only
     compileOnly("org.projectlombok:lombok:${property("lombokVersion")}")
+
+    // Annotation Processors
+    annotationProcessor("org.mapstruct:mapstruct-processor:${property("mapstructVersion")}")
     annotationProcessor("org.projectlombok:lombok:${property("lombokVersion")}")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:${property("lombokMapstructBindingVersion")}")
-    
-    // JWT
-    implementation("io.jsonwebtoken:jjwt-api:${property("jjwtVersion")}")
+
+    // Runtime Only
     runtimeOnly("io.jsonwebtoken:jjwt-impl:${property("jjwtVersion")}")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:${property("jjwtVersion")}")
-    
+
     // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
